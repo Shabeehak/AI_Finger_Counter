7bb4d10fe8b67db2b44faedfe196d14b2f6b1ad2
Shabeehak <shabi.k864@gmail.com>
2025-07-24 20:55:13 +0100

    Final Script added

---
+++ main.py
@@ -0,0 +1,60 @@
+import cv2 as cv
+import mediapipe as mp
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(static_image_mode=False, max_num_hands=1, min_detection_confidence=0.7)
+drawing = mp.solutions.drawing_utils
+
+# GET HANDLAND MARK
+def getHandlandMarks(img, draw):
+    lmlist = []
+    frameRGB = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    handsDetected = hands.process(frameRGB)
+    if handsDetected.multi_hand_landmarks:
+        for landmarks in handsDetected.multi_hand_landmarks:
+            for id, lm in enumerate(landmarks.landmark):
+                # print(id, lm)
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lmlist.append((id, cx, cy))
+                # print(lmlist)
+        if draw:
+            drawing.draw_landmarks(img, landmarks, mpHands.HAND_CONNECTIONS)
+    return lmlist
+
+# FINGER COUNTING
+def fingerCount(lmlist):
+    count=0
+    if lmlist[8][2] < lmlist[6][2]:
+        count+=1
+    if lmlist[12][2] < lmlist[10][2]:
+        count+=1
+    if lmlist[16][2] < lmlist[14][2]:
+        count+=1
+    if lmlist[20][2] < lmlist[18][2]:
+        count+=1
+    if lmlist[4][1] < lmlist[2][1]:
+        count+=1
+    return count
+
+# CAMERA SETUP
+cam = cv.VideoCapture(0)
+while True:
+    success, frame = cam.read()
+    if not success:
+        print('Camera not connected...!')
+        continue
+    frame = cv.flip(frame,1)
+    lmlist = getHandlandMarks(frame, draw=False)
+    if lmlist:
+        # print(lmlist)
+        fc = fingerCount(lmlist=lmlist)
+        # print(fc)
+        h, w, _ = frame.shape
+        cv.rectangle(frame, (w-200,10), (w-10,250), (0,0,0),-1)
+        cv.putText(frame, str(fc), (w-180,240), cv.FONT_HERSHEY_PLAIN, 20, (0,255,255), 30)
+    cv.imshow('AI Finger Counter', frame)
+    if cv.waitKey(1)==ord('q'):
+        break
+cam.release()
+cv.destroyAllWindows()
+hands.close()
